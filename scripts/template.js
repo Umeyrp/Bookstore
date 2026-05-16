0b5eab9170d2e1ef8c272bc5cfd537c52a552040
Muhammed Kemiksiz <62769203+Umeyrp@users.noreply.github.com>
2026-05-16 04:26:30 +0200

    Update image source in template.js for book cards

--- scripts/template.js
+++ scripts/template.js
@@ -5,7 +5,7 @@ function getBooksTemplate(index) {
     return `<article class="book_card">
                 <h2>${books[index].name}</h2>
                 <div class="img_wrapper">
-                    <img src="./assets/img/book.png" alt="">
+                    <img src="${books[index].img}" alt="">
                 </div>
                 <div class="book_informations">
                     <div class="price_likes">
