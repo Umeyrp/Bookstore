48bc0f3f8930c596dfaf4dafb3088d6479ac30e8
Umeyrp <umeyr.kemiksiz@gmail.com>
2026-05-21 09:54:18 +0200

    refactor: added acag for disabled users usage

--- scripts/template.js
+++ scripts/template.js
@@ -2,13 +2,18 @@ function getBooksTemplate(index) {
     return `<article class="book_card">
                 <h2>${books[index].name}</h2>
                 <div class="img_wrapper">
-                    <img src="${books[index].img}" alt="">
+                    <img src="${books[index].img}" alt="Buchcover Iocn">
                 </div>
                 <div class="book_informations">
                     <div class="price_likes">
                         <p>${new Intl.NumberFormat("de-DE", { style: "currency", currency: "EUR" }).format(books[index].price)}</p>
                         <div class="likes_wrapper">
-                            <p><span id="book_likes_number_${index}">${books[index].likes}</span> <img onclick="likeBook(${index})" id="books_likes_img_${index}" src="./assets/icons/heart_${books[index].liked ? "full" : "empty"}.png" alt=""></p>
+                            <p>
+                                <span id="book_likes_number_${index}">${books[index].likes}</span> 
+                                <button type="button" onclick="likeBook(${index})" aria-label="${books[index].liked ? 'Like entfernen' : 'Buch liken'}">
+                                    <img src="./assets/icons/heart_${books[index].liked ? "full" : "empty"}.png"alt="" id="books_likes_img_${index}">
+                                </button>
+                            </p>
                         </div>
                     </div>
                     <div class="information">
@@ -30,8 +35,11 @@ function getBooksTemplate(index) {
                         ${getComments(index)}
                     </div>
                     <div class="send_comment_wrapper">
-                        <input type="text" id="input_text_${index}" name="comment" placeholder="Write a comment..." class="input_comment">
-                        <button onclick="sendComment(${index})"><img src="./assets/icons/send.png"></button>
+                        <label hidden for="input_text_${index}">
+                            Kommentar schreiben
+                        </label>
+                        <input type="text" id="input_text_${index}" name="comment" placeholder="Write a comment..." minlength="3" required class="input_comment">
+                        <button onclick="sendComment(${index})" aria-label="Kommentar senden"><img src="./assets/icons/send.png" alt="send icon"></button>
                         <p class="empty_comment" id="empty_comment_${index}">Bitte schreiben Sie min. 3 Zeichen</p>
                         </div> 
                     
@@ -44,5 +52,5 @@ function getCommentsEmptyTemplate() {
 }
 
 function getCommentsTemplate(index, i) {
-    return `<p><button class="delete_comment_button" onclick="deleteComment(${index},${i})"><img src="./assets/icons/bin.png"></button> <span>${books[index].comments[i].name}</span>: ${books[index].comments[i].comment}</p>`;
+    return `<p><button class="delete_comment_button" onclick="deleteComment(${index},${i})" aria-label="Kommentar löschen"><img src="./assets/icons/bin.png" alt="bin icon for delete"></button> <span>${books[index].comments[i].name}</span>: ${books[index].comments[i].comment}</p>`;
 }
